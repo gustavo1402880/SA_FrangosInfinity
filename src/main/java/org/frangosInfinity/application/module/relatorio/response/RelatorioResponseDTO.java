a18ca8c7b21c73104be255642ae43fb530c5f95f
DANIEL ERTHAL DAPPER <daniel_e_dapper@estudante.sesisenai.org.br>
2026-03-16 20:56:54 -0300

    Feat: Cria Relatorio Response DTO

--- src/main/java/org/frangosInfinity/application/module/relatorio/response/RelatorioResponseDTO.java
+++ src/main/java/org/frangosInfinity/application/module/relatorio/response/RelatorioResponseDTO.java
@@ -1,6 +1,100 @@
 package org.frangosInfinity.application.module.relatorio.response;
 
+import org.frangosInfinity.application.module.mesa.response.MesaResponseDTO;
+import org.frangosInfinity.application.module.relatorio.request.RelatorioRequestDTO;
+
+import java.time.LocalDateTime;
+
 public class RelatorioResponseDTO
 {
+    private LocalDateTime periodoInicio;
+    private LocalDateTime periodoFim;
+    private LocalDateTime dataGeracao;
+    private double totalVendas;
+    private int totalPedidor;
+    private double ticketMedio;
+    private String mensagem;
+    private boolean sucesso;
 
+    public RelatorioResponseDTO() {}
+
+    public LocalDateTime getPeriodoInicio() {
+        return periodoInicio;
+    }
+
+    public void setPeriodoInicio(LocalDateTime periodoInicio) {
+        this.periodoInicio = periodoInicio;
+    }
+
+    public LocalDateTime getPeriodoFim() {
+        return periodoFim;
+    }
+
+    public void setPeriodoFim(LocalDateTime periodoFim) {
+        this.periodoFim = periodoFim;
+    }
+
+    public LocalDateTime getDataGeracao() {
+        return dataGeracao;
+    }
+
+    public void setDataGeracao(LocalDateTime dataGeracao) {
+        this.dataGeracao = dataGeracao;
+    }
+
+    public double getTotalVendas() {
+        return totalVendas;
+    }
+
+    public void setTotalVendas(double totalVendas) {
+        this.totalVendas = totalVendas;
+    }
+
+    public int getTotalPedidor() {
+        return totalPedidor;
+    }
+
+    public void setTotalPedidor(int totalPedidor) {
+        this.totalPedidor = totalPedidor;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public double getTicketMedio() {
+        return ticketMedio;
+    }
+
+    public void setTicketMedio(double ticketMedio) {
+        this.ticketMedio = ticketMedio;
+    }
+
+    public boolean isSucesso() {
+        return sucesso;
+    }
+
+    public void setSucesso(boolean sucesso) {
+        this.sucesso = sucesso;
+    }
+
+    public static RelatorioResponseDTO fromEntity()
+    {
+        RelatorioResponseDTO dto = new RelatorioResponseDTO();
+
+        dto.setPeriodoInicio(dto.getPeriodoInicio());
+        dto.setPeriodoFim(dto.getPeriodoFim());
+        dto.setDataGeracao(dto.getDataGeracao());
+        dto.setTotalVendas(dto.getTotalVendas());
+        dto.setTotalPedidor(dto.getTotalPedidor());
+        dto.setTicketMedio(dto.getTicketMedio());
+        dto.setSucesso(true);
+        dto.setMensagem("Operação realizada com sucesso");
+
+        return dto;
+    }
 }
