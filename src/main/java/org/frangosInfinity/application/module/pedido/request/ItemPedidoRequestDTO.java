373a2cabab3c6b767592d5f0eb1297bd20e731ce
Eduardo Geffert <gefferteduardo@gmail.com>
2026-03-16 20:25:47 -0300

    Feat: Implementa as Request de pedido

--- src/main/java/org/frangosInfinity/application/module/pedido/request/ItemPedidoRequestDTO.java
+++ src/main/java/org/frangosInfinity/application/module/pedido/request/ItemPedidoRequestDTO.java
@@ -1,4 +1,89 @@
 package org.frangosInfinity.application.module.pedido.request;
 
+import org.frangosInfinity.core.entity.module.pedido.ItemPedido;
+
 public class ItemPedidoRequestDTO {
+
+    private Long id_ItemPedido;
+    private Long subPedidoID;
+    private Long produtoid;
+    private Integer quantidade;
+    private Double precoUnitario;
+    private String Observacao;
+    private Double subTotal;
+
+    public ItemPedidoRequestDTO(){};
+
+    public ItemPedidoRequestDTO(Long id_ItemPedido, Long subPedidoID, Long produtoid, Integer quantidade, Double precoUnitario, String observacao, Double subTotal) {
+
+        if(id_ItemPedido >= 1 && subPedidoID >= 1 && produtoid >= 1 && quantidade >= 1 && precoUnitario >= 1 && !observacao.isEmpty() && subTotal >= 1) {
+            this.id_ItemPedido = id_ItemPedido;
+            this.subPedidoID = subPedidoID;
+            this.produtoid = produtoid;
+            this.quantidade = quantidade;
+            this.precoUnitario = precoUnitario;
+            this.Observacao = observacao;
+            this.subTotal = subTotal;
+        }
+
+        throw new IllegalArgumentException("Erro - dados de Item pedido invalidos");
+
+    }
+
+
+    public Long getId_ItemPedido() {
+        return id_ItemPedido;
+    }
+
+    public void setId_ItemPedido(Long id_ItemPedido) {
+        this.id_ItemPedido = id_ItemPedido;
+    }
+
+    public Long getSubPedidoID() {
+        return subPedidoID;
+    }
+
+    public void setSubPedidoID(Long subPedidoID) {
+        this.subPedidoID = subPedidoID;
+    }
+
+    public Long getProdutoid() {
+        return produtoid;
+    }
+
+    public void setProdutoid(Long produtoid) {
+        this.produtoid = produtoid;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getObservacao() {
+        return Observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        Observacao = observacao;
+    }
+
+    public Double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(Double subTotal) {
+        this.subTotal = subTotal;
+    }
 }
--- src/main/java/org/frangosInfinity/application/module/pedido/request/SubPedidoRequestDTO.java
+++ src/main/java/org/frangosInfinity/application/module/pedido/request/SubPedidoRequestDTO.java
@@ -1,4 +1,100 @@
 package org.frangosInfinity.application.module.pedido.request;
 
+import org.frangosInfinity.core.entity.module.pedido.Pedido;
+import org.frangosInfinity.core.enums.StatusPedido;
+
+import java.sql.Date;
+
 public class SubPedidoRequestDTO {
+
+    private Long id;
+    private Pedido pedidoHub;
+    private String clienteID;
+    private Date date;
+    private StatusPedido status;
+    private Double valorTotal;
+    private int tempo_em_minutos;
+    private String obsevacoes;
+
+    public SubPedidoRequestDTO() {}
+
+    public SubPedidoRequestDTO(Long id, Pedido pedidoHub, String clienteID, Date date, StatusPedido status, Double valorTotal, int tempo_em_minutos, String obsevacoes) {
+
+        if(id >= 1 && pedidoHub != null && clienteID.isEmpty() && date.after(Date.valueOf("1900-1-1")) && status != null && valorTotal >= 1 && tempo_em_minutos >= 1 && obsevacoes.isEmpty()) {
+            this.id = id;
+            this.pedidoHub = pedidoHub;
+            this.clienteID = clienteID;
+            this.date = date;
+            this.status = status;
+            this.valorTotal = valorTotal;
+            this.tempo_em_minutos = tempo_em_minutos;
+            this.obsevacoes = obsevacoes;
+        }
+
+        throw new IllegalArgumentException("Erro - os dados do SubPedido estão invalidos");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedidoHub() {
+        return pedidoHub;
+    }
+
+    public void setPedidoHub(Pedido pedidoHub) {
+        this.pedidoHub = pedidoHub;
+    }
+
+    public String getClienteID() {
+        return clienteID;
+    }
+
+    public void setClienteID(String clienteID) {
+        this.clienteID = clienteID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public int getTempo_em_minutos() {
+        return tempo_em_minutos;
+    }
+
+    public void setTempo_em_minutos(int tempo_em_minutos) {
+        this.tempo_em_minutos = tempo_em_minutos;
+    }
+
+    public String getObsevacoes() {
+        return obsevacoes;
+    }
+
+    public void setObsevacoes(String obsevacoes) {
+        this.obsevacoes = obsevacoes;
+    }
 }
