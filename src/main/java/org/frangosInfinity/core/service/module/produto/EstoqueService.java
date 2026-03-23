711a38d9881eea0b5f67f0f20208f13ebddc2e4a
Eduardo Geffert Silva <gefferteduardo@gmail.com>
2026-03-23 19:20:54 -0300

    Feat: Implementa EstoqueController

--- src/main/java/org/frangosInfinity/core/service/module/produto/EstoqueService.java
+++ src/main/java/org/frangosInfinity/core/service/module/produto/EstoqueService.java
@@ -1,16 +1,35 @@
 package org.frangosInfinity.core.service.module.produto;
+import org.frangosInfinity.application.module.produto.request.EstoqueRequestDTO;
+import org.frangosInfinity.application.module.produto.request.ProdutoRequestDTO;
 import org.frangosInfinity.core.entity.module.produto.Estoque;
 import org.frangosInfinity.core.entity.module.produto.Produto;
 import org.frangosInfinity.infrastructure.persistence.connection.ConnectionFactory;
 import org.frangosInfinity.infrastructure.persistence.module.produto.EstoqueDAO;
+import org.frangosInfinity.infrastructure.persistence.module.produto.ProdutoDAO;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class EstoqueService {
 
-    public boolean verifiucarDisponibilidade(int quantidade, Produto produto) throws Exception {
+    private EstoqueDAO estoqueDAO;
 
-        EstoqueDAO estoqueDAO = new EstoqueDAO(ConnectionFactory.getConnection());
+    {
+        try {
+            estoqueDAO= new EstoqueDAO(ConnectionFactory.getConnection());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean verificarDisponibilidade(int quantidade, ProdutoRequestDTO produtoRequestDTO) throws Exception {
+
+
         ArrayList<Estoque> estoques = estoqueDAO.buscarEstoque();
+        Produto produto = new Produto(produtoRequestDTO.getCodigo(),produtoRequestDTO.getNome(),produtoRequestDTO.getDescricao(),
+                produtoRequestDTO.getPreco(),produtoRequestDTO.getCusto(),produtoRequestDTO.getTempoPreparoMinuto(),
+                produtoRequestDTO.isDisponivel(),produtoRequestDTO.getImagemUrl(),produtoRequestDTO.getVendasUltimos30dias(),
+                produtoRequestDTO.getPrecoPendenteAprovacao(),produtoRequestDTO.getCategoria());
 
         for(Estoque e : estoques){
 
@@ -30,18 +49,26 @@ public class EstoqueService {
         throw new Exception("Erro ao verificar disponibilidade");
     }
 
-    public void reduzir(int quantidade, Produto produto,Estoque estoque) throws Exception{
+    public void reduzir(int quantidade, ProdutoRequestDTO produtoRequestDTO, EstoqueRequestDTO estoqueRequestDTO) throws Exception{
 
-        if(quantidade < 0|| produto == null || estoque == null){
+        if(quantidade < 0|| produtoRequestDTO == null || estoqueRequestDTO == null){
 
             throw new Exception("Erro ao diminuir o estoque");
 
         }
-        EstoqueDAO estoqueDAO = new EstoqueDAO(ConnectionFactory.getConnection());
 
-        int quantidadefinal = estoque.getQuantidadeAtual() - quantidade;
+        Produto produto = new Produto(produtoRequestDTO.getCodigo(),produtoRequestDTO.getNome(),produtoRequestDTO.getDescricao(),
+                produtoRequestDTO.getPreco(),produtoRequestDTO.getCusto(),produtoRequestDTO.getTempoPreparoMinuto(),
+                produtoRequestDTO.isDisponivel(),produtoRequestDTO.getImagemUrl(),produtoRequestDTO.getVendasUltimos30dias(),
+                produtoRequestDTO.getPrecoPendenteAprovacao(),produtoRequestDTO.getCategoria());
 
-        if(quantidadefinal < estoque.getQuantidadeMinima()){
+        Estoque estoque = new Estoque(estoqueRequestDTO.getId(),estoqueRequestDTO.getProduto(),
+                estoqueRequestDTO.getQuantidadeAtual(),estoqueRequestDTO.getQuantidadeMinima(),
+                estoqueRequestDTO.getQuantidadeMaxima());
+
+        int quantidadefinal = estoqueRequestDTO.getQuantidadeAtual() - quantidade;
+
+        if(quantidadefinal < estoqueRequestDTO.getQuantidadeMinima()){
 
             throw new Exception("Erro o estoque não pode ser reduzido para menos do estoque minimo");
 
@@ -53,14 +80,22 @@ public class EstoqueService {
 
     }
 
-    public void aumentar(int quantidade, Produto produto, Estoque estoque)throws Exception{
+    public void aumentar(int quantidade, ProdutoRequestDTO produtoRequestDTO, EstoqueRequestDTO estoqueRequestDTO)throws Exception{
 
-        if(quantidade < 0|| produto == null || estoque == null){
+        if(quantidade < 0|| produtoRequestDTO == null || estoqueRequestDTO == null){
 
             throw new Exception("Erro ao aumentar o estoque");
 
         }
-        EstoqueDAO estoqueDAO = new EstoqueDAO(ConnectionFactory.getConnection());
+
+        Produto produto = new Produto(produtoRequestDTO.getCodigo(),produtoRequestDTO.getNome(),produtoRequestDTO.getDescricao(),
+                produtoRequestDTO.getPreco(),produtoRequestDTO.getCusto(),produtoRequestDTO.getTempoPreparoMinuto(),
+                produtoRequestDTO.isDisponivel(),produtoRequestDTO.getImagemUrl(),produtoRequestDTO.getVendasUltimos30dias(),
+                produtoRequestDTO.getPrecoPendenteAprovacao(),produtoRequestDTO.getCategoria());
+
+        Estoque estoque = new Estoque(estoqueRequestDTO.getId(),estoqueRequestDTO.getProduto(),
+                estoqueRequestDTO.getQuantidadeAtual(),estoqueRequestDTO.getQuantidadeMinima(),
+                estoqueRequestDTO.getQuantidadeMaxima());
 
         int quantidadefinal = estoque.getQuantidadeAtual() + quantidade;
 
--- src/main/java/org/frangosInfinity/infrastructure/console/module/produto/controller/EstoqueController.java
+++ src/main/java/org/frangosInfinity/infrastructure/console/module/produto/controller/EstoqueController.java
@@ -1,4 +1,103 @@
 package org.frangosInfinity.infrastructure.console.module.produto.controller;
 
+import org.frangosInfinity.application.module.produto.request.EstoqueRequestDTO;
+import org.frangosInfinity.application.module.produto.request.ProdutoRequestDTO;
+import org.frangosInfinity.core.entity.module.produto.Estoque;
+import org.frangosInfinity.core.entity.module.produto.Produto;
+import org.frangosInfinity.core.service.module.produto.EstoqueService;
+import org.frangosInfinity.infrastructure.persistence.connection.ConnectionFactory;
+import org.frangosInfinity.infrastructure.persistence.module.produto.EstoqueDAO;
+
+import java.sql.SQLException;
+
 public class EstoqueController {
+
+    private EstoqueService estoqueService;
+
+    {
+        try {
+            estoqueService = new EstoqueService();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean verificarDisponibilidade(int quantidade, Produto produto){
+
+        try {
+
+            if (quantidade >= 1) {
+                ProdutoRequestDTO produtoRequestDTO = new ProdutoRequestDTO(produto.getId(),produto.getCodigo(),
+                        produto.getNome(),produto.getDescricao(),produto.getPreco(),produto.getCusto(),
+                        produto.getTempoPreparoMinuto(),produto.getDisponivel(),produto.getImagemUrl(),
+                        produto.getVendasUltimos300dias(),produto.getPrecoPendenteAprovacao(),produto.getCategoria());
+
+                return estoqueService.verificarDisponibilidade(quantidade, produtoRequestDTO);
+            }
+
+            throw new RuntimeException("Erro - quantidade precisa ser positiva!");
+
+        }catch (Exception e){
+
+            throw new RuntimeException(e.getMessage());
+
+        }
+    }
+
+    public void reduzirEstoque(int quantidade, Estoque estoque, Produto produto){
+
+        try {
+
+            if(quantidade >= 1 && estoque != null && produto != null){
+
+                ProdutoRequestDTO produtoRequestDTO = new ProdutoRequestDTO(produto.getId(),produto.getCodigo(),
+                        produto.getNome(),produto.getDescricao(),produto.getPreco(),produto.getCusto(),
+                        produto.getTempoPreparoMinuto(),produto.getDisponivel(),produto.getImagemUrl(),
+                        produto.getVendasUltimos300dias(),produto.getPrecoPendenteAprovacao(),produto.getCategoria());
+
+                EstoqueRequestDTO estoqueRequestDTO = new EstoqueRequestDTO(estoque.getId(),
+                        estoque.getProduto(),estoque.getQuantidadeAtual(),estoque.getQuantidadeMinima(),
+                        estoque.getQuantidadeMaxima());
+
+                estoqueService.reduzir(quantidade,produtoRequestDTO,estoqueRequestDTO);
+
+            }
+
+            throw new RuntimeException("Erro - valores invalidos");
+
+        }catch (Exception e){
+
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public void aumentarestoque(int quantidade, Produto produto, Estoque estoque){
+
+        try {
+
+            if(quantidade >= 1 && estoque != null && produto != null){
+
+                ProdutoRequestDTO produtoRequestDTO = new ProdutoRequestDTO(produto.getId(),produto.getCodigo(),
+                        produto.getNome(),produto.getDescricao(),produto.getPreco(),produto.getCusto(),
+                        produto.getTempoPreparoMinuto(),produto.getDisponivel(),produto.getImagemUrl(),
+                        produto.getVendasUltimos300dias(),produto.getPrecoPendenteAprovacao(),produto.getCategoria());
+
+                EstoqueRequestDTO estoqueRequestDTO = new EstoqueRequestDTO(estoque.getId(),
+                        estoque.getProduto(),estoque.getQuantidadeAtual(),estoque.getQuantidadeMinima(),
+                        estoque.getQuantidadeMaxima());
+
+                estoqueService.aumentar(quantidade,produtoRequestDTO,estoqueRequestDTO);
+
+            }
+
+            throw new RuntimeException("Erro - valores invalidos");
+
+        }catch (Exception e){
+
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+
 }
