858da85f2713666101e15607670ba5cfd3239397
MARCO ANTÔNIO GRASSI <marco_grassi@estudante.sesisenai.org.br>
2026-03-25 16:17:08 -0300

    Fix: Arruma PedidosVIew

--- src/main/java/org/frangosInfinity/infrastructure/console/module/usuario/MenuAtendente.java
+++ src/main/java/org/frangosInfinity/infrastructure/console/module/usuario/MenuAtendente.java
@@ -1,11 +1,12 @@
 package org.frangosInfinity.infrastructure.console.module.usuario;
 
 import org.frangosInfinity.core.enums.NivelAcesso;
+import org.frangosInfinity.infrastructure.console.module.pedido.RegistrarPedidoManual;
 
 public class MenuAtendente
 {
-    public static void menuInicial(long idAdm, NivelAcesso nivelAcesso)
+    public static void menuInicial(long idAtendente)
     {
-
+        RegistrarPedidoManual.RegistrarPedido(idAtendente);
     }
 }
